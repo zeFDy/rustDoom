f1a1da8c7b2cb81a4b0d432d2d439a98dc67b2b2
franck dehornoy <franck.dehornoy@selhagroup.com>
2024-09-06 16:02:42 +0200

    Save 06/09/24 16:02

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(dead_code)]
 
-use std::{fs, io::Write};
+use std::{fs, io::Write, usize};
 use colored::Colorize;
 use std::process::exit;
 use fs::File;
@@ -148,32 +148,136 @@ impl mapProcFile
         }
     }
 
-    pub fn extractModelBlock(&mut self, theLogFile:&mut myLogFile, sBlock:String)
+    pub fn extractModelBlock(&mut self, theLogFile:&mut myLogFile, uiStart:usize, uiStop:usize)
     {
+            let mut uiOffset = uiStart;
+
             //println!("extractModelBlock");
-            theLogFile.log("extractModelBlock\n".to_string());
-            theLogFile.log(sBlock.to_string());
+            theLogFile.log("extractModelBlock ".to_string());
+            //theLogFile.log(sBlock.to_string());
+
+            let sName = self.getNextString(uiOffset, uiStop);
+            theLogFile.log(sName.0.to_string());
+            uiOffset = sName.1;
+
+            //theLogFile.log("\n".to_string());
+            theLogFile.log(", numSurfaces = ".to_string());
+
+            let dNumSurfaces = self.getNextNumber(uiOffset, uiStop);
+            let iNumberOfSurfaces = dNumSurfaces.0 as usize;
+            uiOffset = dNumSurfaces.1;
+
+            theLogFile.log(dNumSurfaces.0.to_string());
+            //theLogFile.log("\nSurfaces :\n".to_string());
+            theLogFile.log("\n".to_string());
+
+            let mut uiSurfaceLoopIndex =0;
+            if iNumberOfSurfaces==0  {return;}
+            loop 
+            {
+                let sSurfaceName = self.getNextString(uiOffset, uiStop);
+                let sMessage = format!("Surface[{}] materialName = ", uiSurfaceLoopIndex);
+                theLogFile.log(sMessage);
+                theLogFile.log(sSurfaceName.0.to_string());
+                uiOffset = sSurfaceName.1;
+
+                let dNumVerts = self.getNextNumber(uiOffset, uiStop);
+                let iNumberOfVertices = dNumVerts.0 as usize;
+                theLogFile.log(", iNumberOfVertices = ".to_string());
+                theLogFile.log(iNumberOfVertices.to_string());
+                uiOffset = dNumVerts.1;
+    
+                let dNumIndexes = self.getNextNumber(uiOffset, uiStop);
+                let iNumberOfIndices = dNumIndexes.0 as usize;
+                theLogFile.log(", dNumberOfIndices = ".to_string());
+                theLogFile.log(iNumberOfIndices.to_string());
+                uiOffset = dNumIndexes.1;
+                theLogFile.log("\n".to_string());
+
+                if iNumberOfVertices>0
+                {
+                    let mut iNumberOfVerticesLoopCounter =0;
+                    loop 
+                    {
+                        for i in 0..8
+                        {
+                            let coord = self.getNextNumber(uiOffset, uiStop);
+                            if i!=0 {theLogFile.log(",".to_string());}
+                            theLogFile.log(coord.0.to_string());
+                            theLogFile.log(" ".to_string());
+                            uiOffset = coord.1;
+                        }
+                        theLogFile.log("\n".to_string());
+
+                        iNumberOfVerticesLoopCounter+=1;
+                        if iNumberOfVerticesLoopCounter>=iNumberOfVertices    {break;};
+                    }    
+                }
+            
+                if iNumberOfIndices>0
+                {
+                    let mut iNumberOfIndicesLoopCounter =0;
+                    loop 
+                    {
+                        let id = self.getNextNumber(uiOffset, uiStop);
+                        if iNumberOfIndicesLoopCounter!=0 {theLogFile.log(",".to_string());}
+                        theLogFile.log(id.0.to_string());
+                        theLogFile.log(" ".to_string());
+                        uiOffset = id.1;
+
+                        iNumberOfIndicesLoopCounter+=1;
+                        if iNumberOfIndicesLoopCounter>=iNumberOfIndices    {break;};
+                    }
+                    theLogFile.log("\n".to_string());
+
+                }
+
+                theLogFile.log("\n".to_string());
+                uiSurfaceLoopIndex += 1;
+
+                if uiSurfaceLoopIndex>=iNumberOfSurfaces    {break;};     
+            };
     }
 
-    pub fn extractInterreaPortalsBlock(&mut self, theLogFile:&mut myLogFile, sBlock:String)
+    pub fn extractInterreaPortalsBlock(&mut self, theLogFile:&mut myLogFile, uiStart:usize, uiStop:usize)
     {
             //println!("extractModelBlock");
             theLogFile.log("extractInterreaPortalsBlock\n".to_string());
-            theLogFile.log(sBlock.to_string());
+            //theLogFile.log(sBlock.to_string());
     }
 
-    pub fn extractNodesBlock(&mut self, theLogFile:&mut myLogFile, sBlock:String)
+    pub fn extractNodesBlock(&mut self, theLogFile:&mut myLogFile, uiStart:usize, uiStop:usize)
     {
             //println!("extractModelBlock");
             theLogFile.log("extractModelBlock\n".to_string());
-            theLogFile.log(sBlock.to_string());
+            //theLogFile.log(sBlock.to_string());
     }
 
-    pub fn extractShadowModelBlock(&mut self, theLogFile:&mut myLogFile, sBlock:String)
+    pub fn extractShadowModelBlock(&mut self, theLogFile:&mut myLogFile, uiStart:usize, uiStop:usize)
     {
             //println!("extractModelBlock");
             theLogFile.log("extractShadowModelBlock\n".to_string());
-            theLogFile.log(sBlock.to_string());
+            //theLogFile.log(sBlock.to_string());
+    }
+
+    pub fn displayBlockData(&mut self, uiStart:usize, uiStop:usize)
+    {
+            let mut uiIndex =uiStart;
+            let mut sString = "".to_string();
+
+            loop 
+            {
+                //print!("{}", self.inputBuffer[uiIndex]);
+                let key:char = self.inputBuffer[uiIndex] as char;
+                sString += &key.to_string();
+
+                if  uiIndex > uiStop
+                {
+                    println!("{}", sString);
+                    break;    
+                }
+                uiIndex +=1;    
+            };
     }
 
     pub fn extractData(&mut self, theLogFile:&mut myLogFile)
@@ -182,21 +286,26 @@ impl mapProcFile
 
         loop
         {
-
-            
             let sBlockName = self.getNextBlockName();
             //println!("{}", sBlockName);
             let sBlockData = self.getNextBlockData();
             //println!("{}", sBlockData);
-            
+
+            /*
+            println!("{}", sBlockData.0);
+            println!("{}", sBlockData.1);
+            self.displayBlockData(sBlockData.0, sBlockData.1);
+            */
+
             match &sBlockName as &str 
             {
-                "model"             =>  self.extractModelBlock(theLogFile, sBlockData),
-                "interreaPortals"   =>  self.extractInterreaPortalsBlock(theLogFile, sBlockData),       //{self.logFile.log("ToDo :: extract interreaPortals\n".to_string());     /*println!("ToDo :: extract interreaPortals");   fs::write("log.txt", "ToDo :: extract interreaPortals") .expect("Could not write to log file");*/ },
-                "nodes"             =>  self.extractNodesBlock(theLogFile, sBlockData),                 //{self.logFile.log("ToDo :: extract nodes\n".to_string());               /*println!("ToDo :: extract nodes");             fs::write("log.txt", "ToDo :: extract nodes") .expect("Could not write to log file");*/ },
-                "shadowModel"       =>  self.extractShadowModelBlock(theLogFile, sBlockData),           //{self.logFile.log("ToDo :: extract shadowModel\n".to_string());         /*println!("ToDo :: extract shadowModel");       fs::write("log.txt", "ToDo :: extract shadowModel") .expect("Could not write to log file");*/ },
+                "model"             =>  self.extractModelBlock(theLogFile, sBlockData.0, sBlockData.1),
+                "interreaPortals"   =>  self.extractInterreaPortalsBlock(theLogFile, sBlockData.0, sBlockData.1),       //{self.logFile.log("ToDo :: extract interreaPortals\n".to_string());     /*println!("ToDo :: extract interreaPortals");   fs::write("log.txt", "ToDo :: extract interreaPortals") .expect("Could not write to log file");*/ },
+                "nodes"             =>  self.extractNodesBlock(theLogFile, sBlockData.0, sBlockData.1),                 //{self.logFile.log("ToDo :: extract nodes\n".to_string());               /*println!("ToDo :: extract nodes");             fs::write("log.txt", "ToDo :: extract nodes") .expect("Could not write to log file");*/ },
+                "shadowModel"       =>  self.extractShadowModelBlock(theLogFile, sBlockData.0, sBlockData.1),           //{self.logFile.log("ToDo :: extract shadowModel\n".to_string());         /*println!("ToDo :: extract shadowModel");       fs::write("log.txt", "ToDo :: extract shadowModel") .expect("Could not write to log file");*/ },
                 _                   =>  break,  //{println!("Unknown block name {}", sBlockName); exit(-2);},
             }
+            
         }
 
         /*
@@ -267,19 +376,20 @@ impl mapProcFile
         sLine
     }
 
-    pub fn getNextString(&mut self) -> String
+    pub fn getNextString(&mut self, uiStart:usize, uiStop:usize) -> (String,usize)
     {
         //let mut   iStatus:i32 = 0;
 		let mut     c:u8;
         let mut     iBracket:i32 = 0;
 		let mut     sNextString:String = "".to_string();
+        let mut     uiOffset = uiStart;
 
 		loop
 		{
-			c = self.inputBuffer[self.readOffset];
+			c = self.inputBuffer[uiOffset /*self.readOffset*/];
             if c == 0u8 {break};
 
-            self.readOffset +=1;
+            uiOffset +=1;   //self.readOffset +=1;
 
 			if c == b'"'	    {iBracket+=1;}
 			if iBracket == 2	{break;}
@@ -293,18 +403,19 @@ impl mapProcFile
                 }
 			}
 
-            if self.readOffset >= self.uiSize
+            //if self.readOffset >= self.uiSize
+            if uiOffset > uiStop
             {
                 self.bEOF = true;
-                return sNextString;
+                return (sNextString, uiOffset);
             }
 
 		}
 
-		sNextString
+		return (sNextString, uiOffset);
     }
 
-    pub fn getNextNumber(&mut self) -> f64
+    pub fn getNextNumber(&mut self, uiStart:usize, uiStop:usize) -> (f64,usize)
     {
         //let mut   iStatus:i32     = 0;
 		let mut     c:u8;
@@ -313,34 +424,38 @@ impl mapProcFile
         let mut     iIsANumber:i32;
         //char		caDummy[10];
         let mut     sNumberAsString:String = "".to_string();
-        
+        let mut     uiOffset = uiStart;
+
     
             loop
             {
-                c = self.inputBuffer[self.readOffset];
+                c = self.inputBuffer[uiOffset /*self.readOffset*/];
                 if c == 0u8 {break};
 
-                if self.readOffset >= self.uiSize
+                //if self.readOffset >= self.uiSize
+                if uiOffset > uiStop
                 {
                     self.bEOF = true;
                     let Result:f64  = sNumberAsString.parse().unwrap();
-                    return Result;
+                    return (Result, uiOffset);
                 }
     
                 if      c == b'/' 
-                    &&  self.inputBuffer[self.readOffset +1] == b'*'
+                    &&  self.inputBuffer[uiOffset+1 /*self.readOffset +1*/] == b'*'
                 {
                     loop
                     {
                         if      c == b'*' 
-                            &&  self.inputBuffer[self.readOffset +1] == b'/'   	{break;}
+                            &&  self.inputBuffer[uiOffset+1 /*self.readOffset +1*/] == b'/'   	{break;}
 
-                        self.readOffset +=1;
-                        c = self.inputBuffer[self.readOffset];
+                        //self.readOffset +=1;
+                        uiOffset+=1;
+
+                        c = self.inputBuffer[uiOffset /*self.readOffset*/];
                     }
                 }
     
-                c = self.inputBuffer[self.readOffset];
+                c = self.inputBuffer[uiOffset /*self.readOffset*/];
                     
                 if (c >= b'0' && c <= b'9') || c == b'.' || c == b'-'
                 {
@@ -367,11 +482,12 @@ impl mapProcFile
                     sNumberAsString += &key.to_string();
                 }
     
-                self.readOffset +=1;
+                //self.readOffset +=1;
+                uiOffset+=1;
             }
     
         let Result:f64  = sNumberAsString.parse().unwrap();
-        Result
+        return (Result, uiOffset);
     }
 
     pub fn getNextBlockName(&mut self) -> String
@@ -405,20 +521,23 @@ impl mapProcFile
         return	sBlockName;
     }
 
-    pub fn getNextBlockData(&mut self) -> String
+    pub fn getNextBlockData(&mut self) -> (usize, usize)
     {
         let mut     c:u8;
-        let mut     sBlockData:String = "".to_string();
+        //let mut   sBlockData:String = "".to_string();
         let mut		iIndent = 0;
         let mut	    bDejaVu = false;
         let mut	    bSendToOutput;
-    
+        let mut     uiStartIndex:usize =0;
+        let mut     uiStopIndex:usize =0;
+        
         loop
         {
             if self.readOffset >= self.uiSize
             {
                 self.bEOF = true;
-                return sBlockData;
+                return (uiStartIndex, uiStopIndex);
+                //return sBlockData;
             }
 
             c = self.inputBuffer[self.readOffset];
@@ -430,7 +549,9 @@ impl mapProcFile
                 if bDejaVu == false
                 {
                     bSendToOutput = false;
+                    uiStartIndex = self.readOffset;
                 }
+
                 bDejaVu=true;
                 iIndent+=1; 
             }
@@ -447,15 +568,17 @@ impl mapProcFile
                         
             if bSendToOutput == true
             {
-                let key:char = c as char;
-                sBlockData += &key.to_string();
+            //    let key:char = c as char;
+            //    sBlockData += &key.to_string();
+                uiStopIndex = self.readOffset;
             }
 
             self.readOffset +=1;
         }
 
-        return	sBlockData;
-    
+        //return	sBlockData;
+        return (uiStartIndex, uiStopIndex);
+                
     }
 }
 
